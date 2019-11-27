7c18eabc665400aeb7fb45d2a3e9dc9c04c04c2b
gaskinsjro <gaskins.30@osu.edu>
2019-11-26 23:35:46 -0500

    Updates

---
+++ precache-manifest.c57bb8dbf2146f422f316fb02bec03fe.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0e97bb58c3e4ae77b04563f4b8eca58",
+    "url": "/francais_trois_projet/index.html"
+  },
+  {
+    "revision": "1dd1efdc7bc5ee0d8cf4",
+    "url": "/francais_trois_projet/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "9d2c60f6a3f1eb3579cd",
+    "url": "/francais_trois_projet/static/js/2.66338377.chunk.js"
+  },
+  {
+    "revision": "1dd1efdc7bc5ee0d8cf4",
+    "url": "/francais_trois_projet/static/js/main.7d1adbdb.chunk.js"
+  },
+  {
+    "revision": "414a671f962601e4319e",
+    "url": "/francais_trois_projet/static/js/runtime-main.d47ccb66.js"
+  },
+  {
+    "revision": "664dec7f426f2b0d36345bb555bd93aa",
+    "url": "/francais_trois_projet/static/media/food.664dec7f.jpg"
+  },
+  {
+    "revision": "a05082584768f177afb93046aa8cb2f1",
+    "url": "/francais_trois_projet/static/media/lyon pic (1).a0508258.jpg"
+  },
+  {
+    "revision": "db8aed2ecaac14acf710e6590c87c99b",
+    "url": "/francais_trois_projet/static/media/trump.db8aed2e.jpg"
+  }
+]);
